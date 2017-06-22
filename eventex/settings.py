a24d951a14bca9cc911a7fb10ea25617f1dc69db
RAFAEL DOS SANTOS REIS <RAFREI@nt.embratel.com.br>
2017-06-22 11:44:48 -0300

    Another error - delete { on line 83 of code

--- eventex/settings.py
+++ eventex/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-    }
 }
 
 
