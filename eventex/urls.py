39c862df1bd5b575a813d01e8aa4bae07e81082f
RAFAEL DOS SANTOS REIS <RAFREI@nt.embratel.com.br>
2017-07-26 15:05:34 -0300

    Tela de inscrição

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
+
 urlpatterns = [
-    url(r'^$', 'eventex.core.views.home'),
+    url(r'^$', home),
+    url(r'inscricao/$', subscribe),
     url(r'^admin/', include(admin.site.urls)),
 ]
